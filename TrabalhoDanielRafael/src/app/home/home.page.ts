b161281eaf7ce7f90646ef082cd0dc547a07451a
WesleiRafael <rafael_velao@hotmail.com>
2023-03-28 20:46:35 -0300

    atualizando as categorias cerveja vinhos e sucos

--- TrabalhoDanielRafael/src/app/home/home.page.ts
+++ TrabalhoDanielRafael/src/app/home/home.page.ts
@@ -15,11 +15,19 @@ export class HomePage {
   ]
 
   public bebidas = [
-    { codigo: 1, categoria: 1, descricao: 'Refrigerante Coca Cola Original - Lata 350 ml - Contém açucar', nome: 'Coca-Cola lata 350ml - Original', valor: 4.99, imagens: ['COCA LATA.jpg'], visibled: false },
-    { codigo: 2, categoria: 1, descricao: 'Refrigerante Fanta Laranja Original - Lata 350 ml - Contém açucar', nome: 'Fanta Laranja lata 350ml - Original', valor: 4.59, imagens: ['FANTA LATA.jpg'], visibled: false },
-    { codigo: 3, categoria: 1, descricao: 'Refrigerante Coca Cola Original PET 1,5L - Contém açucar', nome: 'Coca-Cola 1,5 Litros - Original', valor: 6.99, imagens: ['COCA 1,5 L.png'], visibled: false },
-    { codigo: 4, categoria: 1, descricao: 'Refrigerante Fanta Laranja Original PET 2L - Contém açucar', nome: 'Fanta Laranja 2 Litros - Original', valor: 7.99, imagens: ['FANTA 2L.png'], visibled: false },
-   
+    { codigo: 1,  categoria: 1, descricao: 'Refrigerante Coca Cola Original - Lata 350 ml - Contém açucar',     nome: 'Coca-Cola lata 350ml - Original',     valor: 4.99, imagens: ['COCA LATA.jpg'],  visibled: false },
+    { codigo: 2,  categoria: 1, descricao: 'Refrigerante Fanta Laranja Original - Lata 350 ml - Contém açucar', nome: 'Fanta Laranja lata 350ml - Original', valor: 4.59, imagens: ['FANTA LATA.jpg'], visibled: false },
+    { codigo: 3,  categoria: 1, descricao: 'Refrigerante Coca Cola Original PET 1,5L - Contém açucar',          nome: 'Coca-Cola 1,5 Litros - Original',     valor: 6.99, imagens: ['COCA 1,5 L.png'], visibled: false },
+    { codigo: 4,  categoria: 1, descricao: 'Refrigerante Fanta Laranja Original PET 2L - Contém açucar',        nome: 'Fanta Laranja 2 Litros - Original',   valor: 7.99, imagens: ['FANTA 2L.png'],   visibled: false },
+    { codigo: 5,  categoria: 2, descricao: 'Cerveja Brahma lata 350ml', nome: 'Brahma lata 350ml',         valor: 3.50, imagens: ['COCA LATA.jpg'],  visibled: false },
+    { codigo: 6,  categoria: 2, descricao: 'Cerveja Skol lata 350ml',   nome: 'Cerveja Skol lata 350ml',   valor: 3.50, imagens: ['FANTA LATA.jpg'], visibled: false },
+    { codigo: 7,  categoria: 2, descricao: 'Cerveja Spaten lata 350ml', nome: 'Cerveja Spaten lata 350ml', valor: 4.28, imagens: ['COCA 1,5 L.png'], visibled: false },
+    { codigo: 8,  categoria: 2, descricao: 'Cerveja Amstel lata 350ml', nome: 'Cerveja Amstel lata 350ml', valor: 4.60, imagens: ['FANTA 2L.png'],   visibled: false },
+    { codigo: 9,  categoria: 3, descricao: 'Vinho Malbec 750 ml',                 nome: 'Vinho Malbec 750 ml',                 valor: 35.90, imagens: ['COCA LATA.jpg'],  visibled: false },
+    { codigo: 10, categoria: 3, descricao: 'vinho tinto colina D oro 750 ml',     nome: 'vinho tinto colina D oro 750 ml',     valor: 76.63, imagens: ['FANTA LATA.jpg'], visibled: false },
+    { codigo: 11, categoria: 3, descricao: 'vinho tinto Niágara crevelim 750 ml', nome: 'vinho tinto Niágara crevelim 750 ml', valor: 25.64, imagens: ['COCA 1,5 L.png'], visibled: false },
+    { codigo: 12, categoria: 3, descricao: 'vinho tinto Pergola 750 ml',          nome: 'vinho tinto Pergola 750 ml',          valor: 34.99, imagens: ['FANTA 2L.png'],   visibled: false },
+    
   ]
 
   public slideOpts = {
